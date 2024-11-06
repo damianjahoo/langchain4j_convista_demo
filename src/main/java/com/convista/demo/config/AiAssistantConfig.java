841ea63609e8a5464d9a60c878453ce6d7a897ba
Damian Jachowicz <damian.jachowicz@convista.com>
2024-11-06 19:30:28 +0100

    3. RAG

--- src/main/java/com/convista/demo/config/AiAssistantConfig.java
+++ src/main/java/com/convista/demo/config/AiAssistantConfig.java
@@ -1,18 +1,23 @@
 package com.convista.demo.config;
 
 import com.convista.demo.ai.Assistant;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiAssistantConfig {
     private final ChatLanguageModel model;
+    private final InMemoryEmbeddingStore<TextSegment> embeddingStore;
 
-    public AiAssistantConfig(ChatLanguageModel model) {
+    public AiAssistantConfig(ChatLanguageModel model, InMemoryEmbeddingStore<TextSegment> embeddingStore) {
         this.model = model;
+        this.embeddingStore = embeddingStore;
     }
 
     @Bean
@@ -20,6 +25,7 @@ public class AiAssistantConfig {
         return AiServices.builder(Assistant.class)
                 .chatLanguageModel(model)
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
                 .build();
     }
 }
